3f29b2439856c3ef0cb0679b14ed2c84c204aca8
vachikkh <vachik19941010@gmial.com>
2020-09-21 20:37:08 +0400

    remove anomaly points in altitude

--- logic.c
+++ logic.c
@@ -4,12 +4,17 @@
 #include <iostream>
 #include <chrono>
 #include <unistd.h>
-unsigned long millis()
-{
-//for measuring time
-auto time = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()).time_since_epoch(); 
-return std::chrono::duration_cast<std::chrono::milliseconds>(time).count();
 
+#include <cmath>
+#include <bits/stdc++.h>
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////this part is for anomaly detection from 3 points ///////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+double distance(double a, double b)
+{
+    return abs(a - b);
 }
 
 // Function to sort an array to find the median of an array
@@ -36,6 +41,84 @@ void array_sort(double *array, int n)
     // {
     //     printf("\narray_1[%d] : %f", i, array[i]);
     // }
+}
+
+vector<int> annomalyPoints(double points[])
+{
+    double max, d;
+    int foo1, foo2;
+    //double points[3] = {4, 5, 10}
+    vector<int> annomalies;
+    for (int i = 0; i < 3; i++)
+    {
+        max = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            d = distance(points[i], points[j]);
+            if (d > max)
+            {
+                max = d;
+                foo1 = i;
+                foo2 = j;
+            }
+        }
+
+        annomalies.push_back(foo1);
+        annomalies.push_back(foo2);
+
+        // annomalies[2 * i] = foo1;
+        // annomalies[2 * i + 1] = foo2;
+        // cout <<max<<"  " <<foo1 << " " << foo2 <<endl;
+
+        // cout << max << endl;
+    }
+    // for(int i = 0; i < 6; i++)
+    // cout<<annomalies[i];
+    return annomalies;
+}
+
+int mostFrequent(double arr[], int n)
+{
+    // Sort the array
+    array_sort(arr, n);
+
+    // find the max frequency using linear traversal
+    int max_count = 1, res = arr[0], curr_count = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] == arr[i - 1])
+            curr_count++;
+
+        else
+        {
+            if (curr_count > max_count)
+            {
+                max_count = curr_count;
+                res = arr[i - 1];
+            }
+            curr_count = 1;
+        }
+    }
+
+    // If last element is most frequent
+    if (curr_count > max_count)
+    {
+        max_count = curr_count;
+        res = arr[n - 1];
+    }
+
+    return res;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////anomaly detection ends here ////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+unsigned long millis()
+{
+    //for measuring time
+    auto time = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now()).time_since_epoch();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(time).count();
 }
 
 // function to calculate the median of the array
@@ -73,86 +156,104 @@ bool is_burst(double altitude, double acc)
     // # if the ballon goes too high#
     // ##############################
     if (altitude >= 33333)
-        {
-            printf("balloon released \n");
+    {
+        printf("balloon released \n");
         burst = true;
         return burst;
-        }
+    }
 
     // Sort the array in ascending order
-   double copied[10];
-   int loop;
-   
-   for(loop = 0; loop < 10; loop++) {
-      copied[loop] = acc_list[loop];
-   }
+    double copied[10];
+    int loop;
+
+    for (loop = 0; loop < 10; loop++)
+    {
+        copied[loop] = acc_list[loop];
+    }
     array_sort(copied, n);
 
     // Now pass the sorted array to calculate
     // the median of your array.
     double curr_acc = find_median(acc_list, n);
     printf("curr acc = %f  prev acc = %f", curr_acc, prev_acc);
-    if ((0<=curr_acc && curr_acc<=4) && (0<=prev_acc && prev_acc<=4))
+    if ((0 <= curr_acc && curr_acc <= 4) && (0 <= prev_acc && prev_acc <= 4))
     {
         prev_acc = curr_acc;
-        
-        if (false== free_fall)
-           {
-             start = millis();             
+
+        if (false == free_fall)
+        {
+            start = millis();
             free_fall = true;
-           }
+        }
 
         else if (free_fall)
-            {
+        {
             stop = millis();
-            //  if 1 sec continious free falling 
+            //  if 1 sec continious free falling
             printf("time diff: %lu \n", stop - start);
             if (stop - start > 5000)
-                {
+            {
 
                 printf("descending 1 second in a row of \n");
-                
-                burst = true;
-                 }
-            }
 
+                burst = true;
+            }
+        }
     }
     else
     {
-            printf("not right acc condition----------------------- \n");
-            prev_acc = curr_acc;
-            free_fall = false;
+        printf("not right acc condition----------------------- \n");
+        prev_acc = curr_acc;
+        free_fall = false;
     }
     printf("free fall %d \n", free_fall);
     return burst;
-}   
+}
 
-    
+//  patachute realese logic
 bool parachute_engage = false;
 bool parachute_rel = false;
 bool parachute_relief(double altitude, bool burst)
-{
-    
-    // # condition for descending open up the parachute 
+{ // if ballon goes up from 7000 m then comes down to 5000 m than the parachute is realesed
+
+    // # condition for descending open up the parachute
     if (parachute_engage && altitude <= 5000)
-    {   printf("parachute is open now \n");
+    {
+        printf("parachute is open now \n");
         parachute_rel = true;
     }
     // # check if you once passed 6000 metre means you are ascending
-    if (not parachute_engage && altitude >=7000)
+    if (not parachute_engage && altitude >= 7000)
     {
         printf("you have passed 7000 m, parachute is closed \n");
         parachute_engage = true;
     }
-    // # additional condition 
+    // # additional condition
     if (burst && altitude <= 5000)
         parachute_rel = true;
-    
+
     return parachute_rel;
 }
 
 int main()
-{
+{   
+    //// altitude anomaly detection 
+    double altitudes[3] = {8.0, 9.0, 105.0};  // TODO replace with real altitude data
+    vector<int> annomaly_points;
+
+    annomaly_points.clear();
+    annomaly_points = annomalyPoints(altitudes);
+    double annomaly_points_arr[6];
+    for (int i = 0; i < 6; i++)
+        annomaly_points_arr[i] = annomaly_points[i];
+
+    int n = sizeof(annomaly_points_arr) / sizeof(annomaly_points_arr[0]);
+    int annomaly_ind = mostFrequent(annomaly_points_arr, n);
+    altitudes[annomaly_ind] = 0;
+    double altitude = (altitudes[0] + altitudes[1] + altitudes[2]) / 2;
+    printf("altitude without annomaly %f \n", altitude);
+
+    //////////////////////////// logic part is here /////////////////////
     bool camer_charge = true;
     bool ballon_sep = false;
     bool parachute_rel = false;
@@ -162,9 +263,10 @@ int main()
     // #flag is true if the acceleration is higher than 5
 
     // declare some local variables
-    int acc_mock[32] = {5, 5, 5, 5, 5, 4, 4, 4, 3,3,3,4,9,9,9,9,9,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2};
+    int acc_mock[32] = {5, 5, 5, 5, 5, 4, 4, 4, 3, 3, 3, 4, 9, 9, 9, 9, 9, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
     double alt_mock[5] = {1000, 8000, 20000, 5000, 2000};
-    for(int i = 0; i < 33; i++) {
+    for (int i = 0; i < 33; i++)
+    {
         sleep(1);
         printf("**\n");
         if (not burst)
@@ -174,9 +276,10 @@ int main()
 
         printf("altitude: %f \n", alt_mock[i]);
         // printf("time %lu \n", millis());
-        if (parachute_relief(alt_mock[i], burst)) break;
+        if (parachute_relief(alt_mock[i], burst))
+            break;
     }
-    
+
     // Now pass the sorted array to calculate
     // the median of your array.
 
